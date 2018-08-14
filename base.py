6dc18ef01698ae600f906001acf949225f81743f
alexandru solomes <mariussolomes@gmail.com>
2018-08-14 11:59:17 +0100

    local mbp updates

---
+++ base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class PiBaseRegression(ABC):
+
+    @abstractmethod
+    def __init__(self, X, y):
+        self.n_samples, self.n_features = X.shape
+        self.X = X
+        self.y = y
+        self.coefficients = None
+        super(PiBaseRegression, self).__init__()
+
+    @abstractmethod
+    def fit(self):
+        return self
+
+    @abstractmethod
+    def predict(self):
+        return self
---
+++ linear_model/__init__.py
@@ -0,0 +1,2 @@
+from . import LinearRegression
+from LinearRegression import PiLinearRegression
