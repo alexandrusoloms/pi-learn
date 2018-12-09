ff3a06535f489bf5fa73445c6523dbf17e7081be
Alexandru-Marius Solomes <alexandrusoloms@outlook.com>
2018-12-09 20:16:32 +0000

    adding clustering to init

--- pilearn/__init__.py
+++ pilearn/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ['linear_model', 'preprocessing', 'classification', 'non_linear_model']
+__all__ = ['linear_model',
+           'preprocessing',
+           'classification',
+           'non_linear_model',
+           'clustering']
